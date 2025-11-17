a6d85cade3f5545a08d15ab9d89839a776533b44
cifuentesrubiela <cifuentesrubiela978@gmail.com>
2025-11-16 22:46:32 -0500

    add:se creo el seed.controller.ts

---
+++ back/src/database/seed.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post, HttpCode } from '@nestjs/common';
+import { SeedService } from './seed.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+@ApiTags('Seed')
+@Controller('seed')
+export class SeedController {
+  constructor(private readonly seedService: SeedService) {}
+
+  // Endpoint para poblar la base de datos
+  @Post()
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Poblar la base de datos con datos de ejemplo' })
+  @ApiResponse({ status: 200, description: 'Base de datos poblada exitosamente' })
+  async run() {
+    return await this.seedService.seed();
+  }
+}
